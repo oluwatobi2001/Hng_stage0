125adbcfa6037eb499e09c8b296f2ed8a14f43a5
oluwatobi2001 <tobilyn77@gmail.com>
2025-01-31 08:06:50 +0100

    Project files

---
+++ index.js
@@ -0,0 +1,33 @@
+const express  =  require('express');
+const app= express();
+
+
+const cors = require('cors') ;
+const bodyParser = require('body-parser') 
+
+const dotenv = require('dotenv'); 
+
+
+
+dotenv.config();
+app.use(express.json());
+app.use(cors());
+
+
+const time = new  Date().toISOString();
+
+
+ app.use(bodyParser.json());
+ app.use("/", (req, res) => {
+
+     res.status(200).json({
+        "email": "tobilyn77@gmail.com",
+        "current_datetime" : time,
+        "github_url": "<https://github.com/oluwatobi12001/Hng_stage0.git>"
+     })
+ })
+
+
+ app.listen(process.env.PORT  || 5000, ()=> {
+    console.log("tobi is king");
+})
